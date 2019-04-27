25895d79518eb9f2e314b5352094ddeba08f36ff
Rukshan Ranatunge <shan@Rukshans-MacBook-Pro.local>
2019-04-27 14:21:53 +0530

    bug fix

--- www/js/app.js
+++ www/js/app.js
@@ -11,20 +11,21 @@ $('#post').click(function(e){
 
 	var source   = document.getElementById("feed-template").innerHTML;
 	var template = Handlebars.compile(source);
-	var newfdata = {poke: data, poke_time: moment(time, 'MMMM Do YYYY, h:mm:ss a').fromNow()};
+	var newfdata = [{poke: data, poke_time: moment(time, 'MMMM Do YYYY, h:mm:ss a').fromNow()}];
     var html = template(newfdata);
     $('#render-feed').prepend(html);
     $('#msg').val('');
 
-	$.ajax({
-		method: 'POST',
-		data: {message: data, dtime: time},
-		url: url
-	})
+	// $.ajax({
+	// 	method: 'POST',
+	// 	data: {message: data, dtime: time},
+	// 	url: url
+	// })
 })
 
 // A $( document ).ready() block.
 $( document ).ready(function() {
+    var n = 1;
 	var source   = document.getElementById("feed-template").innerHTML;
 	var template = Handlebars.compile(source);
     
@@ -43,5 +44,26 @@ $( document ).ready(function() {
     		var html = template(feed_data);
     		$('#render-feed').html(html);
     	}
+    })
+
+    $('#lmore').click(function(){
+        var url = 'https://podi.pw/api/more_pokes'
+        $.ajax({
+            method: 'get',
+            url: url,
+            data: {n: n},
+            success: function(data){
+                if (data) {
+                    if (data == []) {return}
+                    var newfdata = JSON.parse(data);
+                    for (var i = feed_data.length - 1; i >= 0; i--) {
+                        feed_data[i]['poke_time'] = moment(feed_data[i]['poke_time'], 'MMMM Do YYYY, h:mm:ss a').fromNow();
+                    }
+                    var html = template(feed_data);
+                    $('#render-feed').append(html);
+                    n++;
+                }
+            }
+        })
     })
 });
